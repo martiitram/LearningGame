e5abb7e449876b1c3b5cb5d53cc455599340572a
itram <itram@DESKTOP-36BIT7Q>
2020-11-29 21:41:24 +0100

    commit uncomited files of last commit

---
+++ LearningGame/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody playerRigidBody;
+
+    public int velocity = 1;
+    
+    void Start()
+    {
+        player = this.gameObject;
+        playerRigidBody = player.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = new Vector3(0, 0, 0);
+        if (Input.GetKey("up"))
+        {
+            direction += new Vector3(0,0,1);
+        }
+        if (Input.GetKey("down"))
+        {
+            direction += new Vector3(0,0,-1);
+        }
+        if (Input.GetKey("right"))
+        {
+            direction += new Vector3(1,0,0);
+        }
+        if (Input.GetKey("left"))
+        {
+            direction += new Vector3(-1,0,0);
+        }
+        playerRigidBody.velocity = direction * velocity;
+    }
+}
