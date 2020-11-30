58bda41c45e3cfccbf6958a7c56202196d29573a
itram <itram@DESKTOP-36BIT7Q>
2020-11-30 19:41:56 +0100

    Add main menu

---
+++ LearningGame/Assets/Scripts/MainMenuScreenBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScreenBehaviour : MonoBehaviour
+{
+    public Button m_playButton;
+    void Start()
+    {
+        m_playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void Update()
+    {
+        
+    }
+}
