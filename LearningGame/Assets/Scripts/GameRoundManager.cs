23563c6431a627865d493c214193184871cea646
ferran coma rosell <ferrancoma@hotmail.com>
2020-11-30 19:44:09 +0100

    ui gameplay

---
+++ LearningGame/Assets/Scripts/GameRoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoundManager : MonoBehaviour
+{
+    private int m_amoutOfObjectives;
+    private float m_currentTime;
+    private GameObject[] m_goalSpawnPoints;
+    private GameObject m_uiGameObjectRoot;
+
+    public GameObject m_goalsPrefab;
+    public int m_timeInSeconds;
+
+    void Awake()
+    {
+        if (m_goalSpawnPoints == null)
+            m_goalSpawnPoints = GameObject.FindGameObjectsWithTag("goalSpawnPoint");
+
+        foreach (GameObject spawnPoint in m_goalSpawnPoints)
+        {
+            ++m_amoutOfObjectives;
+            Destroy(spawnPoint);
+            Instantiate(m_goalsPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        }
+
+        m_currentTime = 0;
+    }
+    
+    public int GetAmountOfObjectives()
+    {
+        return m_amoutOfObjectives;
+    }
+
+    public float GetReminingTime()
+    {
+        return m_timeInSeconds * 1000 - m_currentTime * 1000;
+    }
+
+    void Update()
+    {
+        m_currentTime += Time.deltaTime;
+    }
+}
---
+++ LearningGame/Assets/Scripts/GameRoundUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoundUI : MonoBehaviour
+{
+    private UnityEngine.UI.Text m_goalText;
+    private UnityEngine.UI.Text m_timeText;
+    private GameRoundManager m_gameRoundManager;
+
+    void Start()
+    {
+        m_goalText = gameObject.transform.Find("GoalText").GetComponent<UnityEngine.UI.Text>();
+        m_timeText = gameObject.transform.Find("TimerText").GetComponent<UnityEngine.UI.Text>();
+        m_gameRoundManager = GameObject.FindGameObjectWithTag("GameRoundManager").GetComponent<GameRoundManager>();
+        SetTime(m_gameRoundManager.GetReminingTime());
+        SetObjective();
+    }
+
+    public void SetObjective()
+    {
+        m_goalText.text = "0/" + m_gameRoundManager.GetAmountOfObjectives().ToString();
+    }
+
+    void Update()
+    {
+        SetTime(m_gameRoundManager.GetReminingTime());
+    }
+
+    void SetTime(float timeInMilisecs)
+    {
+        int minutes = (int)(timeInMilisecs / 60 / 1000);
+        string minutesText = minutes.ToString();
+        minutesText = minutes < 10 ? "0" + minutesText : minutesText;
+        timeInMilisecs = timeInMilisecs - (minutes * 60 * 1000);
+
+        int seconds = (int)(timeInMilisecs / 1000);
+        string secondsText = seconds.ToString();
+        secondsText = seconds < 10 ? "0" + secondsText : secondsText;
+        
+        m_timeText.text = minutesText + ":" + secondsText;
+    }
+}
