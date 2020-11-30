1bd00db18730567984403335c34ce98d39797fe6
ferran coma rosell <ferrancoma@hotmail.com>
2020-11-30 22:28:59 +0100

    rename goal to objective

--- LearningGame/Assets/Scripts/ObjectiveBehaviour.cs
+++ LearningGame/Assets/Scripts/ObjectiveBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalObjetBehaviour : MonoBehaviour
+public class ObjectiveBehaviour : MonoBehaviour
 {
     private GameObject goalObject;
 
